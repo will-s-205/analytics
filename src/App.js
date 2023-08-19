91fb17769fab959764c114be682d61e1b9c3f67f
William S <will.step.205@gmail.com>
2023-08-19 12:12:47 -0700

    initial grid

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import './App.scss';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <div className="App">
+
+        <div className="header">
+          <h1>Summer referral competition</h1>
+          <div className="filter-days-bar"></div>
+        </div>
+
+        <div className="main">
+          <div className='wrapper grid'>
+            <div className='participants participants-grid-item'></div>
+            <div className='insights insights-grid-item'></div>
+            <div className='leaderboard minor-grid-item'></div>
+            <div className='traffic minor-grid-item'></div>
+            <div className='location minor-grid-item'></div>
+            <div className='behaviour minor-grid-item'></div>
+          </div>
+        </div>
+
+        {/* <div className="footer">
+          <p>by William Step</p>
+        </div> */}
+      </div>
+    </div>
   );
 }
 
