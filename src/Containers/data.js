a0e8c2af93d9fd8b88a425117c1ccbbad912fb66
William S <will.step.205@gmail.com>
2023-08-19 19:41:59 -0700

    Create data.js

---
+++ src/Containers/data.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        "country": "USA",
+        "city": "Mountain View",
+        "emoji": "🇺🇸",
+        "faang_company": "Google",
+        "email": "john.doe@gmail.com",
+        "sign_ups": 3524,
+        "friends_invites": 178
+    },
+    {
+        "country": "Ireland",
+        "city": "Dublin",
+        "emoji": "🇮🇪",
+        "faang_company": "Facebook",
+        "email": "mary.smith@facebook.com",
+        "sign_ups": 2571,
+        "friends_invites": 232
+    },
+    {
+        "country": "USA",
+        "city": "Cupertino",
+        "emoji": "🇺🇸",
+        "faang_company": "Apple",
+        "email": "alex.apple@icloud.com",
+        "sign_ups": 6290,
+        "friends_invites": 450
+    },
+    {
+        "country": "India",
+        "city": "Hyderabad",
+        "emoji": "🇮🇳",
+        "faang_company": "Amazon",
+        "email": "priya.amazon@gmail.com",
+        "sign_ups": 7589,
+        "friends_invites": 312
+    },
+    {
+        "country": "USA",
+        "city": "Menlo Park",
+        "emoji": "🇺🇸",
+        "faang_company": "Meta",
+        "email": "lucas.fb@hotmail.com",
+        "sign_ups": 5821,
+        "friends_invites": 258
+    },
+    {
+        "country": "USA",
+        "city": "Mountain View",
+        "emoji": "🇺🇸",
+        "faang_company": "YouTube",
+        "email": "bob.dodo@gmail.com",
+        "sign_ups": 354,
+        "friends_invites": 78
+    },
+]
