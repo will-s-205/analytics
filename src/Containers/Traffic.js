a133326843cbdfec92ed43b7daee6cd15fc2956b
William S <will.step.205@gmail.com>
2023-08-19 20:37:09 -0700

    traffic data render

--- src/Containers/Traffic.js
+++ src/Containers/Traffic.js
@@ -1,9 +1,38 @@
 import '../App.scss';
+import data from './data.js';
+
+function trafficData() {
+
+    return data
+        // .slice(0, 5)
+        .sort((a, b) => b.sign_ups - a.sign_ups)
+        .map(({ faang_company, sign_ups }) => {
+            return (
+                <div className='data'>
+                    <label className='label'>{faang_company}</label>
+                    <label className='number'>{sign_ups.toLocaleString('en-US')}</label>
+                </div>
+            )
+        })
+}
 
 function Traffic() {
     return (
         <div className='traffic minor-grid-item'>
-            <p>Traffic</p>
+
+            <div className='header-container'>
+                <h4>Traffic</h4>
+                <div className='buttons'>
+                    <button className='button'>Source</button>
+                    <button className='button'>City</button>
+                </div>
+            </div>
+
+            <div className='select'>
+                {trafficData()}
+            </div>
+
+            <button className='button'>See traffic sources</button>
         </div>
     );
 }
